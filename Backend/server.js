3f4e93e67b157022d02d9874a4a16f4e514f3c14
rakeshk326 <rakeshkittu2002@gmail.com>
2024-06-01 17:48:25 +0530

    Final Changes

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const orderRouter = require('./routes/order');
 const cookieParser = require('cookie-parser');
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
@@ -18,10 +18,16 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(checkForAuthenticationCookie("token"));
 
-mongoose.connect('mongodb+srv://kprakesh2002:sJH9NTNw9zjW4UFV@yummy-foods-db.ferbn29.mongodb.net/yummy?retryWrites=true&w=majority&appName=yummy-foods-db')
-.then(console.log("MongoDB connected"));
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error("MongoDB connection error:", err));
 
 app.use("/user", userRouter);
 app.use("/order", orderRouter);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
 
 app.listen(PORT, () => console.log(`Server running at port : ${PORT}`));
