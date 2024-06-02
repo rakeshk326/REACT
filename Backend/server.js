3cef3f325aee58c00df9af24fb20566760ae8b0f
rakeshk326 <rakeshkittu2002@gmail.com>
2024-06-02 18:10:34 +0530

    Final changes done

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8000;
-const cors = require('cors');
 require('dotenv').config();
 const mongoose = require("mongoose");
 const userRouter = require('./routes/user');
@@ -10,24 +10,26 @@ const cookieParser = require('cookie-parser');
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(checkForAuthenticationCookie("token"));
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error("MongoDB connection error:", err));
 
 app.use("/user", userRouter);
 app.use("/order", orderRouter);
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
+app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "React app URL"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
 
 app.listen(PORT, () => console.log(`Server running at port : ${PORT}`));
